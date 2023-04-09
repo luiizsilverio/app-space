5339b151662aa58c9eb709008ca9b006a762891d
luiizsilverio <luiiz.silverio@gmail.com>
2023-04-09 16:45:10 -0300

    Configurando o projeto no Firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,8 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { initializeAuth, getReactNativePersistence } from "firebase/auth/react-native";
 
 import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from "@env"
 
@@ -22,4 +24,8 @@ const db = getFirestore(app);
 // Inicializa o Storage
 const storage = getStorage(app);
 
-export { db, storage };
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+})
+
+export { db, storage, auth };
--- src/telas/Principal/index.js
+++ src/telas/Principal/index.js
@@ -1,39 +1,52 @@
-import { View, ScrollView, Image } from "react-native";
+import { View, ScrollView } from "react-native";
 import { useEffect, useState } from "react";
 
 import { Cabecalho } from "../../componentes/Cabecalho";
 import { CartaoInfo } from "../../componentes/CartaoInfo";
 import { NovoPostBotao } from "../../componentes/NovoPostBotao";
 import { pegarPostsTempoReal } from "../../servicos/firestore";
+import { logout } from "../../servicos/auth";
 import estilos from "./estilos";
 
 export default function Principal({ navigation }) {
     const [posts, setPosts] = useState([]);
-    const [urlImage, setUrlImage] = useState('');
+    const [notifications, setNotifications] = useState([]);
 
-    useEffect(() => {
-      pegarPostsTempoReal(setPosts);
-    },[])
+  function mostrarNotificacoes(){
+    navigation.navigate("Notificacoes", { notificacoes: notifications });
+    setNotifications([]);
+  }
 
-    return (
-        <View style={estilos.container}>
-            <Cabecalho />
+  useEffect(() => {
+    pegarPostsTempoReal(setPosts);
+  },[])
 
-            <ScrollView style={estilos.scroll} showsVerticalScrollIndicator={false}>
+  return (
+    <View style={estilos.container}>
+      <Cabecalho 
+        quantidadeNotificacoes={notifications.length}
+        onPress={mostrarNotificacoes}
+        logout={() => {
+          logout();
+          navigation.replace("Login");
+        }}
+      />
 
-                {posts?.map((item) => (
-                    <CartaoInfo 
-                        key={item.id} 
-                        titulo={item.titulo}  
-                        fonte={item.fonte} 
-                        descricao={item.descricao} 
-                        imagem={item?.imagemUrl}
-                        acao={() => navigation.navigate("Post", { item })}
-                    />
-                ))}
-            </ScrollView>
+      <ScrollView style={estilos.scroll} showsVerticalScrollIndicator={false}>
 
-            <NovoPostBotao acao={() => navigation.navigate("Post")} />
-        </View>
-    );
+        {posts?.map((item) => (
+          <CartaoInfo 
+              key={item.id} 
+              titulo={item.titulo}  
+              fonte={item.fonte} 
+              descricao={item.descricao} 
+              imagem={item?.imagemUrl}
+              acao={() => navigation.navigate("Post", { item })}
+          />
+        ))}
+      </ScrollView>
+
+      <NovoPostBotao acao={() => navigation.navigate("Post")} />
+    </View>
+  );
 }
